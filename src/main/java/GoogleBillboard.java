51fc0eccb946ea7a12eaf48bbae7438783fb25fe
aljow <143014720+aljow@users.noreply.github.com>
2024-01-17 09:38:22 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -2,8 +2,8 @@
 
 public class GoogleBillboard {
 	public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
-	public final static String nums; 
-	public final static double othNum; 
+	public String nums; 
+	public double othNum; 
 	public static void main(final String[] args) {
 		for (int i = 2; i < e.length()-10; i++) {
     			nums = e.substring(i,10+i);
